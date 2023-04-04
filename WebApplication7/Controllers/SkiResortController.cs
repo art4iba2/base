4c4ffb9b60fd40d065474dd030016381019dbbd0
user <user@LAPTOP-P6JDQUOH>
2023-04-04 22:25:04 +0400

    Добавьте файлы проекта.

---
+++ WebApplication7/Controllers/SkiResortController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+public class SkiResortController : Controller
+{
+    private static List<SkiResortModel> skiResorts = new List<SkiResortModel>()
+    {
+        new SkiResortModel { Id = 1, Name = "Роза Хутор", City = "Сочи", OpenDate = new DateTime(2011, 11, 24), NumberOfSkiSlopes = 102, AverageSlopeLength = 1350, AnnualRevenue = 2100000000 },
+        new SkiResortModel { Id = 2, Name = "Гора Аибга", City = "Красная поляна", OpenDate = new DateTime(2014, 1, 4), NumberOfSkiSlopes = 75, AverageSlopeLength = 1590, AnnualRevenue = 1700000000 },
+        new SkiResortModel { Id = 3, Name = "Эльбрус", City = "Терек", OpenDate = new DateTime(1958, 12, 28), NumberOfSkiSlopes = 11, AverageSlopeLength = 4400, AnnualRevenue = 600000000 }
+    };
+
+    private void AddSkiResortToList(SkiResortModel skiResort)
+    {
+        skiResort.Id = skiResorts.Count + 1;
+        skiResorts.Add(skiResort);
+    }
+
+
+    public ActionResult Index()
+    {
+        return View(skiResorts.ToList());
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(SkiResortModel skiResort)
+    {
+        if (ModelState.IsValid)
+        {
+            AddSkiResortToList(skiResort);
+            return RedirectToAction("Index");
+        }
+        return View(skiResort);
+    }
+
+}
+
---
+++ WebApplication7/Models/SkiResortModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class SkiResortModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Название курорта")]
+        public string Name { get; set; }
+
+        [DisplayName("Город")]
+        public string City { get; set; }
+
+        [DisplayName("Дата открытия")]
+        [DataType(DataType.Date)]
+        public DateTime OpenDate { get; set; }
+
+        [DisplayName("Количество горнолыжных трасс")]
+        public int NumberOfSkiSlopes { get; set; }
+
+        [DisplayName("Средняя длина трассы")]
+        public double AverageSlopeLength { get; set; }
+
+        [DisplayName("Ежегодный общий доход")]
+        public decimal AnnualRevenue { get; set; }
+    }
+}
